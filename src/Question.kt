af4c3b22f448feb6ccb4502c5653a1bd49bd20ce
Firsoova08 <foxalishca@mail.ru>
2026-02-01 18:06:50 +0300

    Use a singleton object

--- src/Question.kt
+++ src/Question.kt
@@ -9,6 +9,34 @@ enum class Difficulty
     MEDIUM,
     HARD
 }
+ object StudentProgress {
+     var total: Int = 10
+     var answered: Int = 3
+ }
+ class Quiz {
+
+     val question1 = Question<String>(
+
+     questionText = "Речка спятила с ума По домам пошла сама...",
+     answer = "водопровод",
+     Difficulty.MEDIUM
+     )
+     val question2 = Question<Boolean>(
+         questionText = "Небо зелёное. Правда или ложь",
+         answer= false,
+         Difficulty.EASY
+     )
+     val question3 = Question<Int>(
+         questionText = "Сколько дней между полнолуниями?",
+         answer = 28,
+         Difficulty.HARD
+     )
+     companion object StudentProgress {
+         var total: Int = 10
+         var answered: Int = 3
+     }
+
+ }
 fun main() {
     val question1 = Question<String>(
         questionText = "Речка спятила с ума По домам пошла сама.___",
@@ -25,8 +53,10 @@ fun main() {
         answer = 28,
         Difficulty.HARD
     )
-    println(question1.answer)
-    println(question2.answer)
-    println(question3.answer)
-    println(question1.toString())
+//    println(question1.answer)
+//    println(question2.answer)
+//    println(question3.answer)
+//    println(question1.toString())
+//    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
